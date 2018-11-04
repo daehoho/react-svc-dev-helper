620de4f2eb1bef4143dcb749a25a9d332b9b6eb8
gaeulautumn <gaeulautumnlee@gmail.com>
2018-11-04 11:26:21 +0900

    add Calendar

--- src/components/TeamCommContents.js
+++ src/components/TeamCommContents.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import Calendar from './Calendar';
 
 class TeamCommContents extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedDays: []
+        }
+    }
+
+    handleCreate = (data) => {
+        // data : selectedDays
+        console.log(data);
+    }
+
     render() {
         return (
-            <div>
-                팀커~~
+            <div class="container" id="CONTENTS">
+                <h4>팀커 도우미 입니다 :)</h4>
+                <div class="row card-panel">
+                    <Calendar onCreate={this.handleCreate}/>
+                </div>
             </div>
         );
     }
